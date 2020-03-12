a3961b7134fc886bc191e6a5edf181cc0516301e
ployarisa11 <Ploy_arisa112@hotmail.com>
2020-03-12 18:59:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,9 +37,13 @@ app.get('/', (req, res) => {
     
     const admin = require("firebase-admin");
     admin.initializeApp({
-        apiKey: 'AIzaSyA5KFIcemUtm1_i64TUyifV0WfKjbm9irk',
-        authDomain: 'rru-connect-epeevr.firebaseapp.com',
-        projectId: 'rru-connect-epeevr'
+        apiKey: "AIzaSyA5KFIcemUtm1_i64TUyifV0WfKjbm9irk",
+    authDomain: "rru-connect-epeevr.firebaseapp.com",
+    databaseURL: "https://rru-connect-epeevr.firebaseio.com",
+    projectId: "rru-connect-epeevr",
+    storageBucket: "rru-connect-epeevr.appspot.com",
+    messagingSenderId: "898597223567",
+    appId: "1:898597223567:web:8cd2b5064e8edbaf20c4fa"
       });
 
 
