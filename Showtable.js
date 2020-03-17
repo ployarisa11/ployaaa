e4784f6b9e6eb822c698cb8e7c52f90b37b0b189
ployarisa11 <Ploy_arisa112@hotmail.com>
2020-03-17 13:29:51 +0700

    ppp

--- Showtable.js
+++
@@ -1,9 +0,0 @@
-function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
-}
-
-var number = getUrlVars()["id1"];
