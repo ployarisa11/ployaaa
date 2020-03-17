bc6115e4867bc5a29441000e6a42b32067a93851
ployarisa11 <Ploy_arisa112@hotmail.com>
2020-03-17 14:01:04 +0700

    ppp

--- index.js
+++ index.js
@@ -1,41 +1,82 @@
-const express = require('express')
-const PORT = process.env.PORT || 5000
-var app = express();
-var fire = require('./fire')
-var cors = require('cors');
-var bodyParser = require('body-parser');
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const firebase = require("firebase/app")
+require("firebase/firestore")
+
+var cron = require('node-cron');
+const express = require('express')
+const app = express()
+
 
 app.get('/', (req, res) => {
-  res.send(
-    '<h1>Tes Express & Firebase Cloud Firestore</h1><ul><li><p><b>GET /data/esp8266</b></p></li><li><p><b>GET /data/esp32</b></p></li><li><p><b>GET /data/mkr1000</b></p></li><li><p><b>POST /data/esp8266</b>  => {suhu, lembab, analog}</p></li><li><p><b>POST /data/esp32</b>  => {suhu, lembab, analog}</p></li><li><p><b>POST /data/mkr1000</b>  => {suhu, lembab, analog}</p></li></ul>')
-    const db = fire.firestore();
-    db.collection("Calendar").doc('Topic').collection("ภาคการศึกษาปกติ").orderBy("date", "desc").get().then((snapshot) => {
-      snapshot.forEach(doc => {
-          console.log(doc.id);
-       
-      });
-    });
- 
-})
-
-
-
-
-  app.post('/webhook', (req, res)=>{
-    const db = fire.firestore();
-    db.collection("Calendar").doc('Topic').collection("ภาคการศึกษาปกติ").orderBy("date", "desc").get().then((snapshot) => {
-      snapshot.forEach(doc => {
-          console.log(doc.id);
-          res.send(doc.id);
-      });
-    });
+    res.json({ message: 'Ahoy!' })
   })
 
+//firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyA5KFIcemUtm1_i64TUyifV0WfKjbm9irk",
+    authDomain: "rru-connect-epeevr.firebaseapp.com",
+    databaseURL: "https://rru-connect-epeevr.firebaseio.com",
+    projectId: "rru-connect-epeevr",
+    storageBucket: "rru-connect-epeevr.appspot.com",
+    messagingSenderId: "898597223567",
+    appId: "1:898597223567:web:8cd2b5064e8edbaf20c4fa"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-app.listen(PORT, () => {
-  console.log(`Listening on ${ PORT }`)
-})
+const db = firebase.firestore()
+
+const LineNotify = require("./src/client");
+
+const ACCESS_TOKEN = "GU1WHyUZz9MopIWb4Z7g1soJT7dkuVUMAO3Drjhvy4y";
+const notify = new LineNotify(`${ACCESS_TOKEN}`);
+
+
+
+//เอาชื่อมา
+const line = require('@line/bot-sdk');
+
+const client = new line.Client({
+    channelAccessToken: 'UC877QAnBp4CE5A/3/LNgpLduTrTxEdqZRuF8ShT6p+PFg5A471cvw8buhkpTcRuRbRw3egWF+Y68p2k13FdY9OuYzONaFuYo2zWlDUiJMMHIKjtbqyENtxM1/X2xyjde+OkQyVnJy/6xKNXohqwdQdB04t89/1O/w1cDnyilFU='
+});
+
+
+
+
+
+
+let Noti = db.collection('Count');
+
+cron.schedule('* * * * *', function () {
+    Noti.get().then((snapshot) => {
+        let pp = snapshot.docs[snapshot.docs.length - 1];
+        let noti = pp.data().count;
+
+        if (noti == 1) {
+            client.getProfile('U6d5ea64790214c58808993455916422f')
+                .then((profile) => {
+
+                    notify.sendText("กรุณาติดต่อกลับผู้ใช้งาน " + profile.displayName  + "  ด้วยค่ะ");
+
+                })
+                .catch((err) => {
+                    // error handling
+                });
+
+            Noti.doc('H0pK45spljceEu7DBwmP').update({
+                count: 0
+            });
+
+
+        } else {
+            console.log(pp.data().count);
+        }
+
+    });
+    console.log('running a task every two minutes');
+});
+
+
+app.listen(9000, () => {
+    console.log('Application is running on port 9000')
+  })
