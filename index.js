e10da2f2ee6b57eb0ea1c7257eb1bb5073468249
ployarisa11 <Ploy_arisa112@hotmail.com>
2020-04-17 09:33:20 +0700

    up

--- index.js
+++ index.js
@@ -1,82 +1,263 @@
+/*
+  Webhook of Dialogflow
+  @author: NottDev
+  date: 31/05/2019
+*/
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const port = process.env.PORT || 4000;
+const admin = require('firebase-admin');
 
-const firebase = require("firebase/app")
-require("firebase/firestore")
+let serviceAccount = require('Admin_SDK.json');
 
-var cron = require('node-cron');
-const express = require('express')
-const app = express()
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
+let db = admin.firestore();
+
+
+// Import the appropriate class
+const {
+  WebhookClient
+} = require('dialogflow-fulfillment');
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Ahoy!' })
-  })
+  res.send({
+    success: true
+  });
+})
 
-//firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyA5KFIcemUtm1_i64TUyifV0WfKjbm9irk",
-    authDomain: "rru-connect-epeevr.firebaseapp.com",
-    databaseURL: "https://rru-connect-epeevr.firebaseio.com",
-    projectId: "rru-connect-epeevr",
-    storageBucket: "rru-connect-epeevr.appspot.com",
-    messagingSenderId: "898597223567",
-    appId: "1:898597223567:web:8cd2b5064e8edbaf20c4fa"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+app.post('/webhook', (req, res) => {
+  console.log('POST: /');
+  console.log('Body: ',req.body);
 
-const db = firebase.firestore()
+  //Create an instance
+  const agent = new WebhookClient({
+    request: req,
+    response: res
+  });
 
-const LineNotify = require("./src/client");
-
-const ACCESS_TOKEN = "GU1WHyUZz9MopIWb4Z7g1soJT7dkuVUMAO3Drjhvy4y";
-const notify = new LineNotify(`${ACCESS_TOKEN}`);
-
-
-
-//เอาชื่อมา
-const line = require('@line/bot-sdk');
-
-const client = new line.Client({
-    channelAccessToken: 'UC877QAnBp4CE5A/3/LNgpLduTrTxEdqZRuF8ShT6p+PFg5A471cvw8buhkpTcRuRbRw3egWF+Y68p2k13FdY9OuYzONaFuYo2zWlDUiJMMHIKjtbqyENtxM1/X2xyjde+OkQyVnJy/6xKNXohqwdQdB04t89/1O/w1cDnyilFU='
-});
+  //Test get value of WebhookClient
+  console.log('agentVersion: ' + agent.agentVersion);
+  console.log('intent: ' + agent.intent);
+  console.log('locale: ' + agent.locale);
+  console.log('query: ', agent.query);
+  console.log('session: ', agent.session);
 
 
 
 
-
-
-let Noti = db.collection('Count');
-
-cron.schedule('* * * * *', function () {
-    Noti.get().then((snapshot) => {
-        let pp = snapshot.docs[snapshot.docs.length - 1];
-        let noti = pp.data().count;
-
-        if (noti == 1) {
-            client.getProfile('U6d5ea64790214c58808993455916422f')
-                .then((profile) => {
-
-                    notify.sendText("กรุณาติดต่อกลับผู้ใช้งาน " + profile.displayName  + "  ด้วยค่ะ");
-
-                })
-                .catch((err) => {
-                    // error handling
-                });
-
-            Noti.doc('H0pK45spljceEu7DBwmP').update({
-                count: 0
-            });
-
-
-        } else {
-            console.log(pp.data().count);
+  //Function Location
+  let user_id = request.body.originalDetectIntentRequest.payload.data.source.userId;
+  let pay = {
+      "type": "imagemap",
+      "baseUrl": "https://s3-ap-southeast-1.amazonaws.com/img-in-th/9c33fe1533a8315572294fcdbe714231.jpg?_ignored=",
+      "altText": "แบบประเมินความพึงพอใจ",
+      "baseSize": {
+        "width": 1040,
+        "height": 554
+      },
+      "actions": [
+        {
+          "type": "message",
+          "area": {
+            "x": 19,
+            "y": 247,
+            "width": 163,
+            "height": 297
+          },
+          "text": "มีความพึงพอใจต่อระบบน้อยที่สุด"
+        },
+        {
+          "type": "message",
+          "area": {
+            "x": 237,
+            "y": 249,
+            "width": 155,
+            "height": 293
+          },
+          "text": "มีความพึงพอใจต่อระบบน้อย"
+        },
+        {
+          "type": "message",
+          "area": {
+            "x": 434,
+            "y": 252,
+            "width": 175,
+            "height": 289
+          },
+          "text": "มีความพึงพอใจต่อระบบปานกลาง"
+        },
+        {
+          "type": "message",
+          "area": {
+            "x": 650,
+            "y": 252,
+            "width": 154,
+            "height": 286
+          },
+          "text": "มีความพึงพอใจต่อระบบมาก"
+        },
+        {
+          "type": "message",
+          "area": {
+            "x": 855,
+            "y": 254,
+            "width": 164,
+            "height": 283
+          },
+          "text": "มีความพึงพอใจต่อระบบมากที่สุด"
         }
+      ]
+      };
+  
+  
+      let pay1 = new Payload(`LINE`, pay, { sendAsMessage: true });
 
-    });
-    console.log('running a task every two minutes');
+  let date = new Date();
+  //Count_Accuracy
+  let Count_Intent = admin.firestore().collection("Count_Intent").doc(date.toLocaleDateString());
+
+
+
+  //หมวดการลงทะเบียน
+  //การเพิ่ม
+  function Additional_Credit_Registration(agent) {
+      let payload = {
+          "type": "template",
+          "altText": "this is a confirm template",
+          "template": {
+              "type": "confirm",
+              "actions": [
+                  {
+                      "type": "message",
+                      "label": "ใช่",
+                      "text": "ช่วยตอบคำถามในเรื่องการเพิ่มรายวิชา"
+                  },
+                  {
+                      "type": "message",
+                      "label": "ไม่ใช่",
+                      "text": "ไม่ช่วยตอบคำถามในเรื่องการเพิ่มรายวิชา"
+                  }
+              ],
+              "text": "น้องบ๊อตช่วยตอบคำถามของท่านใช่หรือไม่?"
+          }
+      };
+
+      //ประกาศตัวแปร payload เพื่อแสดงออกหน้าจอ
+      let payload่json = new Payload(`LINE`, payload, { sendAsMessage: true });
+      let text = request.body.queryResult.queryText;
+      let c = 'ช่วยตอบคำถามในเรื่องการเพิ่มรายวิชา';
+      let b = 'ไม่ช่วยตอบคำถามในเรื่องการเพิ่มรายวิชา';
+
+      //Count_Accuracy
+      let Count_Accuracy = admin.firestore().collection("Count_Accuracy").doc(date.toLocaleDateString()).collection("การลงทะเบียน").doc("การเพิ่มรายวิชา");
+      Count_Accuracy.get().then(function (docs) {
+          if (!docs.exists) {
+              Count_Accuracy.set({
+                  ใช่: 0,
+                  ไม่ใช่: 0
+              });
+          }
+
+      });
+
+
+      if (text === c) {
+          agent.add("กรุณากดดาวเพื่อประเมินความพึงพอใจหลังการใช้ระบบ")
+          agent.add(pay1);
+          db.runTransaction(t => {
+              return t.get(Count_Accuracy).then(doc => {
+                  let newcount = doc.data().ใช่ + 1;
+
+                  t.update(Count_Accuracy, {
+                      ใช่: newcount,
+
+                  });
+              });
+          });
+
+
+
+      }
+
+      else if (text === b) {
+
+          agent.add("กรุณากดปุ่มติดต่อเจ้าหน้าที่");
+          db.runTransaction(t => {
+              return t.get(Count_Accuracy).then(doc => {
+                  let newcount = doc.data().ไม่ใช่ + 1;
+
+                  t.update(Count_Accuracy, {
+                      ไม่ใช่: newcount,
+
+                  });
+              });
+          });
+
+      }
+      else {
+          //เชคว่ามี ตัว doc อยู๋ไหม ถ้าไม่ ก็ set ค่า 
+          // count_intent 
+          Count_Intent.get().then(function (docs) {
+              if (!docs.exists) {
+                  Count_Intent.set({
+                      การลงทะเบียน: 1
+                  });
+              }
+
+              else {
+                  //เลขcount เวลามีคนเข้ามาสอบถาม
+
+                  let transaction = db.runTransaction(t => {
+                      return t.get(Count_Intent).then(doc => {
+                          if (doc.data().การลงทะเบียน > 0) {
+                              let newcount = doc.data().การลงทะเบียน + 1;
+
+                              t.update(Count_Intent, {
+                                  การลงทะเบียน: newcount,
+
+                              });
+                          } else {
+                              t.update(Count_Intent, {
+                                  การลงทะเบียน: 1
+                              });
+
+                          }
+
+                      });
+                  });
+              }
+          });
+
+
+          //return ข้อมูลคำตอบ 
+
+          return admin.firestore().collection('Registration').doc('Topic').collection('การเพิ่มรายวิชา').orderBy("date", "desc").limit(1).get().then((snapshot) => {
+              snapshot.forEach(doc => {
+                  agent.add("การเพิ่มรายวิชา\n" + doc.data().description);
+                  agent.add(payload่json); //แสดง paylaod
+                  //agent.add(date.toLocaleDateString());
+
+              });
+          });
+
+      }
+  }
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+
+  intentMap.set("Additional_Credit_Registration", Additional_Credit_Registration);//การเพิ่ม
+  agent.handleRequest(intentMap);
 });
 
-
-app.listen(9000, () => {
-    console.log('Application is running on port 9000')
-  })
+app.listen(port, () => {
+  console.log(`Server is running at port: ${port}`);
+});
